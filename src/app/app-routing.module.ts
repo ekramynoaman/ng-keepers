1218620388efcc6cf328c7ad35656df02fbd0ee1
EkramyNo3man <Ekramy.no3man@gmail.com>
2020-03-20 22:29:50 +0200

    Generate modules and components - Shared Module - Layouts Module - User, Admin,Auth, & Blank comonents

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,29 @@
+import { AuthLayoutComponent } from './shared/components/layouts/auth-layout/auth-layout.component';
+import { AdminLayoutComponent } from './shared/components/layouts/admin-layout/admin-layout.component';
+import { UserLayoutComponent } from './shared/components/layouts/user-layout/user-layout.component';
+import { BlankLayoutComponent } from './shared/components/layouts/blank-layout/blank-layout.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: BlankLayoutComponent,
+  },
+  {
+    path: 'auth',
+    component: AuthLayoutComponent,
+  },
+  {
+    path: 'user',
+    component: UserLayoutComponent,
+  },
+  {
+    path: 'admin',
+    component: AdminLayoutComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { NgModule } from '@angular/core';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ToastrModule } from 'ngx-toastr';
+
+import { SharedModule } from './shared/shared.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,7 +19,8 @@ import { AppComponent } from './app.component';
     AppRoutingModule,
     NgbModule,
     BrowserAnimationsModule, // required animations module
-    ToastrModule.forRoot() // ToastrModule added
+    ToastrModule.forRoot(),
+    SharedModule // ToastrModule added
   ],
   providers: [],
   bootstrap: [AppComponent]
